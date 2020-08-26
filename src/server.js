7871bd3f08e4ccc5b9ade072c93475cea740e957
manojkumardesai <manojkumardbe@gmail.com>
2020-08-26 22:57:31 +0530

    completed auth

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
+import { signup, signin, protect } from './utils/auth'
 import { connect } from './utils/db'
+import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
+import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -14,7 +17,14 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
-app.use('/api/item/', itemRouter);
+
+app.post('/signup', signup)
+app.post('/signin', signin)
+
+app.use('/api', protect)
+app.use('/api/user', userRouter)
+app.use('/api/item', itemRouter)
+app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
