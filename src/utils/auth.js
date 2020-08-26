dd2d113ce69caadf7f44dff1ad98b12bfe4f0e7a
manojkumardesai <manojkumardbe@gmail.com>
2020-08-26 23:06:59 +0530

    completed setup

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -62,7 +62,7 @@ export const signin = async (req, res) => {
 
 export const protect = async (req, res, next) => {
   const bearer = req.headers.authorization
-
+  console.log(req.headers);
   if (!bearer || !bearer.startsWith('Bearer ')) {
     return res.status(401).end()
   }
