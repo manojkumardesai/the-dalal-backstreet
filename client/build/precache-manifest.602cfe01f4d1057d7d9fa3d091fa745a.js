370ae9206384bdd1d50c4f23e4c89ad94dfdccdf
manojkumardesai <manojkumardbe@gmail.com>
2020-08-27 13:42:19 +0530

    fixed deployment issues

---
+++ client/build/precache-manifest.602cfe01f4d1057d7d9fa3d091fa745a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfb6a77cfaa9b36a06271ae419e357c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f5d85be6b4323cce63",
+    "url": "/static/css/main.7970e4fb.chunk.css"
+  },
+  {
+    "revision": "25525f5775110b05ea33",
+    "url": "/static/js/2.2d3322cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d3322cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f5d85be6b4323cce63",
+    "url": "/static/js/main.1371d883.chunk.js"
+  },
+  {
+    "revision": "de74f4ac5356bfb2ea98",
+    "url": "/static/js/runtime-main.a67a8a20.js"
+  },
+  {
+    "revision": "b82ea35f197bf4e3fb34cfdca030f872",
+    "url": "/static/media/bg_welcome.b82ea35f.jpg"
+  }
+]);
