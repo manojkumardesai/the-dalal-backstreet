fb820089f5453f86080315c89ac5fa8343117b6b
manojkumardesai <manojkumardbe@gmail.com>
2020-08-27 18:08:48 +0530

    test styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Home.css";
 // import axios from "axios";
 
 import Registration from "./auth/Registration";
@@ -30,10 +31,11 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-        <h1>Status: {this.props.loggedInStatus}</h1>
+      <div className="wrapper">
+        <h1>Sign Up | Login</h1>
+        { this.props.loggedInStatus === 'LOGGED_IN' && 
         <button onClick={() => this.handleLogoutClick()}>Logout</button>
+        }
         <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
         <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
